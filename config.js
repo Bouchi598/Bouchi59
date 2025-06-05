ed3058ff1c0feddce0ce45bac6207ba9fcfd4dcb
Bouchi598 <elsimo3222@gmail.com>
2025-06-05 19:58:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://admin:motdepasse123@cluster0.abcd.mongodb.net/maBase?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
